cc39972cee366c4580d5149f4a25cda3c20e8595
Abuka13 <abukaozbekbay@gmail.com>
2026-02-03 02:23:59 +0500

    Updating node_modules and connecting to DB

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-
-const connectDB = require("./app/config/db.config");
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./app/config/db.config');
 
 const app = express();
 
-
-app.use(cors());
 app.use(express.json());
-
-
-app.use("/api/auth", require("./app/routes/auth.routes"));
-
 
 connectDB();
 
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
